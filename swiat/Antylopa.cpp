fb1068bfdb169255da684a86df7d012ef7a5a1ed
Redroy44 <redroy44@gmail.com>
2015-04-07 23:04:23 +0200

    akcja dziala z antylopoł

--- swiat/Antylopa.cpp
+++ swiat/Antylopa.cpp
@@ -11,15 +11,15 @@ Antylopa::Antylopa()
 Antylopa::~Antylopa(){}
 
 void Antylopa::akcja(Organizm * organizm[20][20]) {
-	Organizm *tmp = organizm[x][y];
+	Organizm *tmp = organizm[(x - 7)/3][y]; // 7 to X
 	int x1 = x;
-	organizm[x][y] = NULL;
+	organizm[(x - 7) / 3][y] = NULL;
 	int a = rand() % 4;
-	if (a == 0){ x1 = x + 1; x += 3; }
-	else if (a == 1){ x1 = x - 1; x -= 3; }
+	if (a == 0){ x1 = (x - 7) / 3 + 1; x += 3; }
+	else if (a == 1){ x1 = (x - 7) / 3 - 1; x -= 3; }
 	else if (a == 2){ y += 1; }
 	else{ y -= 1; }
-	organizm[x1][y] = tmp;
+	organizm[(x1 - 7) / 3][y] = tmp;
 	tmp = NULL;
 
 }
