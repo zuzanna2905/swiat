f22a982135e7e3e57e7f417871c4bc7fd4cbe67b
Zuzanna <zuzpusie@student.pg.gda.pl>
2015-04-12 11:02:45 +0200

    rozmnazanie

--- swiat/Zwierze.cpp
+++ swiat/Zwierze.cpp
@@ -1,5 +1,14 @@
 #pragma once
 #include "Zwierze.h"
+#include "Antylopa.h"
+#include "Guarana.h"
+#include "Lis.h"
+#include "Mlecz.h"
+#include "Owca.h"
+#include "Trawa.h"
+#include "Wilcze_jagody.h"
+#include "Wilk.h"
+#include "Zolw.h"
 #include <random>
 #define X 7
 #define Y 6
@@ -8,6 +17,10 @@ void Zwierze::akcja(Organizm* organizm[20][20]){
 	Organizm *tmp = organizm[(x - 7) / 3][y - 6]; // 7 to X
 	int x1 = (x - 7) / 3;
 	int y1 = y - 6;
+	int x_old = x;
+	int y_old = y;
+	int x1_old = x1;
+	int y1_old = y1;
 	int a = rand() % 4;
 	gotoxy(x, y);
 	if (a == 0 && x < 64){
@@ -42,109 +55,110 @@ void Zwierze::akcja(Organizm* organizm[20][20]){
 		if (kolizja(organizm, x1, y1))
 		{
 			organizm[x1][y1] = tmp;
+			organizm[x1][y1]->set_tura(true);
+		}
+		else{
+			organizm[x1_old][y1_old] = tmp;
+			organizm[x1_old][y1_old]->set_tura(true);
+			x = x_old;
+			y = y_old;
 		}
 	}
 	else {
 		organizm[x1][y1] = tmp;
+		organizm[x1][y1]->set_tura(true);
 	}
-	organizm[x1][y1]->set_tura(true);
 	tmp = NULL;
 	delete tmp;
 }
 
 bool Zwierze::kolizja(Organizm* organizm[20][20], int x, int y){
-		//if (id == organizm[x][y]->get_id())
-		//{
-		//	for (int i = x; i < 20 && i > 0;){
-		//		for (int j = y; j < 20 && j >0;){
-		//			//int z = rand() % 8;
-		//			if (//z = 0 && 
-		//				organizm[i+1][j] == NULL){
-		//				stworz(organizm, id, i, j);
-		//				return false;
-		//			}
-		//			else if (//z = 1 && 
-		//				organizm[i - 1][j] == NULL){
-		//				stworz(organizm, id, i, j);
-		//				return false;
-		//			}
-		//			else if (//z = 2 && 
-		//				organizm[i + 1][j + 1] == NULL){
-		//				stworz(organizm, id, i, j);
-		//				return false;
-		//			}
-		//			else if (//z = 3 && 
-		//				organizm[i + 1][j - 1] == NULL){
-		//				stworz(organizm, id, i, j);
-		//				return false;
-		//			}
-		//			else if (//z = 4 && 
-		//				organizm[i - 1][j + 1] == NULL){
-		//				stworz(organizm, id, i, j);
-		//				return false;
-		//			}
-		//			else if (//z = 5 && 
-		//				organizm[i - 1][j - 1] == NULL){
-		//				stworz(organizm, id, i, j);
-		//				return false;
-		//			}
-		//			else if (//z = 6 && 
-		//				organizm[i][j - 1] == NULL){
-		//				stworz(organizm, id, i, j);
-		//				return false;
-		//			}
-		//			else if (//z = 7 && 
-		//				organizm[i][j + 1] == NULL){
-		//				stworz(organizm, id, i, j);
-		//				return false;
-		//			}
-		//			j++;
-		//		} i++;
-		//	}
-		//}
-		//else {
-		//	if (this->si³a > organizm[x][y]->get_si³a()){
-		//		return true;
-		//	}
-		//}
+	if (id == organizm[x][y]->get_id())
+	{
+		bool nowy = false;
+		while (!nowy){
+			int z = rand() % 8;
+			if (z == 0 && x < 19 && organizm[x + 1][y] == NULL){
+				stworz(organizm, id, x+1, y);
+				nowy = true;
+			}
+			else if (z == 1 && x > 0 && organizm[x - 1][y] == NULL){
+				stworz(organizm, id, x-1, y);
+				nowy = true;
+			}
+			else if (z == 2 && x < 19 && y <19 && organizm[x + 1][y + 1] == NULL){
+				stworz(organizm, id, x+1, y+1);
+				nowy = true;
+			}
+			else if (z == 3 && x < 19 && y >0 && organizm[x + 1][y - 1] == NULL){
+				stworz(organizm, id, x+1, y-1);
+				nowy = true;
+			}
+			else if (z == 4 && x> 0 && y <19 && organizm[x - 1][y + 1] == NULL){
+				stworz(organizm, id, x-1, y+1);
+				nowy = true;
+			}
+			else if (z == 5 && x> 0 && y >0 && organizm[x - 1][y - 1] == NULL){
+				stworz(organizm, id, x-1, y-1);
+				nowy = true;
+			}
+			else if (z == 6 && y >0	&&	organizm[x][y - 1] == NULL){
+				stworz(organizm, id, x, y-1);
+				nowy = true;
+			}
+			else if (z == 7 &&  y < 19 && organizm[x][y + 1] == NULL){
+				stworz(organizm, id, x, y+1);
+				nowy = true;
+			}
+			//else {
+			//	nowy = true;
+			//}
+		}
+		return false;
+	}
+		else {
+			if (this->si³a > organizm[x][y]->get_si³a()){
+				return true;
+			}
+		}
 		return true;
 }
 
-//void Zwierze::stworz(Organizm* organizm[20][20], int m, int x, int y){
-//		switch (m)
-//		{
-//		case 1:
-//			organizm[x][y] = new Antylopa(m);
-//			organizm[x][y]->set_x(3 * x + X);
-//			organizm[x][y]->set_y(y + Y);
-//			organizm[x][y]->set_tura(false);
-//			organizm[x][y]->rysowanie();
-//			break;
-//		case 2:
-//			organizm[x][y] = new Wilk(m);
-//			organizm[x][y]->set_x(3 * x + X);
-//			organizm[x][y]->set_y(y + Y);
-//			organizm[x][y]->set_tura(false);
-//			organizm[x][y]->rysowanie();
-//			break;
-//		case 3:
-//			organizm[x][y] = new Owca(m);
-//			organizm[x][y]->set_x(3 * x + X);
-//			organizm[x][y]->set_y(y + Y);
-//			organizm[x][y]->set_tura(false);
-//			organizm[x][y]->rysowanie();
-//			break;
-//		case 4:
-//			organizm[x][y] = new Zolw(m);
-//			organizm[x][y]->set_x(3 * x + X);
-//			organizm[x][y]->set_y(y + Y);
-//			organizm[x][y]->rysowanie();
-//			break;
-//		case 5:
-//			organizm[x][y] = new Lis(m);
-//			organizm[x][y]->set_x(3 * x + X);
-//			organizm[x][y]->set_y(y + Y);
-//			organizm[x][y]->rysowanie();
-//			break;
-//	}
-//}
+void Zwierze::stworz(Organizm* organizm[20][20], int m, int x, int y){
+		switch (m)
+		{
+		case 1:
+			organizm[x][y] = new Antylopa(m);
+			organizm[x][y]->set_x(3 * x + X);
+			organizm[x][y]->set_y(y + Y);
+			organizm[x][y]->set_tura(false);
+			organizm[x][y]->rysowanie();
+			break;
+		case 2:
+			organizm[x][y] = new Wilk(m);
+			organizm[x][y]->set_x(3 * x + X);
+			organizm[x][y]->set_y(y + Y);
+			organizm[x][y]->set_tura(true);
+			organizm[x][y]->rysowanie();
+			break;
+		case 3:
+			organizm[x][y] = new Owca(m);
+			organizm[x][y]->set_x(3 * x + X);
+			organizm[x][y]->set_y(y + Y);
+			organizm[x][y]->set_tura(false);
+			organizm[x][y]->rysowanie();
+			break;
+		case 4:
+			organizm[x][y] = new Zolw(m);
+			organizm[x][y]->set_x(3 * x + X);
+			organizm[x][y]->set_y(y + Y);
+			organizm[x][y]->rysowanie();
+			break;
+		case 5:
+			organizm[x][y] = new Lis(m);
+			organizm[x][y]->set_x(3 * x + X);
+			organizm[x][y]->set_y(y + Y);
+			organizm[x][y]->rysowanie();
+			break;
+	}
+}
--- swiat/Zwierze.h
+++ swiat/Zwierze.h
@@ -1,18 +1,10 @@
 #pragma once
 #include "Organizm.h" 
 #include "Conio2.h"
-#include "Antylopa.h"
-#include "Guarana.h"
-#include "Lis.h"
-#include "Mlecz.h"
-#include "Owca.h"
-#include "Trawa.h"
-#include "Wilcze_jagody.h"
-#include "Wilk.h"
-#include "Zolw.h"
+
 
 class Zwierze : public Organizm {
-	// void stworz(Organizm* organizm[20][20], int m, int x, int y);
+	 void stworz(Organizm* organizm[20][20], int m, int x, int y);
 public:
 	virtual void akcja(Organizm *organizm[20][20]); 
 	virtual bool kolizja(Organizm* organizm[20][20], int x, int y);
